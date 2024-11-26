efb8a745af275ced11629b5189687de74d55ed98
danffholden <danielfaridfearnholden@gmail.com>
2024-11-27 03:49:22 +0800

    Records became case class. Checked to make sure data parsed correctly.

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -3,12 +3,10 @@ import scala.io.Source
 import scala.collection.mutable.ListBuffer
 //Created by:
 //  DANIELFARID FEARN HOLDEN 22047880
-//  Current commit: 5
+//  Current commit: 6
 //
 //Project Comments:
-//  Forgot to update comments for previous commit (4)
-//  Still facing issues with Date format, however, new Date type can be tested from java.time.LocalDate API
-//  Found using https://louiseforbes101.medium.com/dates-and-time-in-scala-bb7590276dfe
+//  Finally figured out correct Date data type to use, and so we can test to see whether the data is being stored correctly into records_listBuffer
 //
 //  Updated To-Do:
 //    --Create outline for how to solve the 3 problems--
@@ -17,61 +15,62 @@ import scala.collection.mutable.ListBuffer
 //        https://alvinalexander.com/scala/how-to-open-read-text-files-in-scala-cookbook-examples/
 //      which describes how to read data from a text/data file.
 //
-//     1st step almost done, need to store Date. Time to test LocalDate API.
+//    As described above, Date can now be stored so let's run tests to see whether .csv is being parsed into records_listBuffer correctly
 //
-//Task: Test how to read data from hospital.csv
-//  Since we are just testing to see how the data can be parsed, no emphasis will be placed on efficiency.
-//  As described from assignment paper and in the .csv file, the data is in following format, so create a class with related properties.
-//    1. Date - Date <-- Figure out how to store this one.
-//    2. State - String
-//    3. Beds - Int
-//    ... the rest of which are Int
+//Task: Store .csv data into records_listBuffer. Check that it is done properly with a simple test of a counter and property assessor.
+//Also, update Record to become case class as all properties are values and thus Records will be stored as distinct values.
 
-class Record(val date: LocalDate,
-             val state: String,
-             val Beds: Int,
-             val beds_covid: Int,
-             val beds_nonCrit: Int,
-             val admitted_pui: Int,
-             val admitted_covid: Int,
-             val admitted_total: Int,
-             val discharged_pui: Int,
-             val discharged_covid: Int,
-             val discharged_total: Int,
-             val hosp_covid: Int,
-             val hosp_pui: Int,
-             val hosp_nonCovid: Int
+
+case class Record(
+             date: LocalDate,
+             state: String,
+             Beds: Int,
+             beds_covid: Int,
+             beds_nonCrit: Int,
+             admitted_pui: Int,
+             admitted_covid: Int,
+             admitted_total: Int,
+             discharged_pui: Int,
+             discharged_covid: Int,
+             discharged_total: Int,
+             hosp_covid: Int,
+             hosp_pui: Int,
+             hosp_nonCovid: Int
             )
-end Record
 
 @main def myfirstscala(): Unit =
-  val dateString = "2020-04-24"
-  println(LocalDate.parse(dateString))
+  val dataFilePath = "./data/hospital.csv"
+  val data = Source.fromFile(dataFilePath)
+  val records_listBuffer = ListBuffer.empty[Record] // listBuffer used to store all Record instances efficiently.
+  var counter = 0
+  for (line <- data.drop(1)) { //read each line from hospital.csv file
+    val line_data = line.split(",")
+    val date_format = "yyyy-MM-dd"
+    records_listBuffer.append(Record(
+      LocalDate.parse(line_data(0)),
+      line_data(1),
+      line_data(2).toInt,
+      line_data(3).toInt,
+      line_data(4).toInt,
+      line_data(5).toInt,
+      line_data(6).toInt,
+      line_data(7).toInt,
+      line_data(8).toInt,
+      line_data(9).toInt,
+      line_data(10).toInt,
+      line_data(11).toInt,
+      line_data(12).toInt,
+      line_data(13).toInt)
+    )
+    counter += 1
+  }
+  data.close()
 
-  val datesArr : Array[LocalDate] = Array(LocalDate.parse("2020-06-24"), LocalDate.parse("2020-04-25"), LocalDate.parse("2020-05-24"))
-  println(datesArr.mkString)
-  println(datesArr.sorted.mkString) //not only is data in correct format, but we can sort chronogically should we want to, thus, java.time.LocalDate shall be API used.
 
-//  val hospitalData = "./data/hospital.csv"
-//  val records_listBuffer = ListBuffer.empty[Record] // listBuffer used to store all Record instances efficiently.
-//
-//  for (line <- Source.fromFile(hospitalData).getLines.drop(1)) { //read each line from hospital.csv file
-//    val line_data = line.split(",")
-//    val date_format = "yyyy-MM-dd"
-//    records_listBuffer.append(Record(
-//      Date(), // too much code to simply parse the string into a Date format with the same information. Must find a different Date type/API to use.
-//      line_data(1),
-//      line_data(2).toInt,
-//      line_data(3).toInt,
-//      line_data(4).toInt,
-//      line_data(5).toInt,
-//      line_data(6).toInt,
-//      line_data(7).toInt,
-//      line_data(8).toInt,
-//      line_data(9).toInt,
-//      line_data(10).toInt,
-//      line_data(11).toInt,
-//      line_data(12).toInt,
-//      line_data(13).toInt)
-//    )
-//  }
+//by manually opening and scrolling to the bottom of the hospital.csv file, we can see that there are 26031 rows of record data.
+//so that means if we print the counter, we should see 26031
+  println(counter) //output is 26031
+
+//let us also check that the data is in the correct format by checking the first few data of the first record in records_listBuffer
+  println(s"${records_listBuffer.head.date}\n${records_listBuffer.head.state}\n${records_listBuffer.head.Beds}\n${records_listBuffer.head.beds_covid}\n${records_listBuffer.head.beds_nonCrit}")
+  // output is correct with what is expected, thus we can begin accessing these data to manipulate them to get the answers.
